ed37c06097ad0caf74f03a04608db28d99b5fb55
qawqawzse <qawqawzse@gmail.com>
2018-04-04 16:29:13 +0300

    message input -start

--- ctrl.js
+++ ctrl.js
@@ -1,3 +1,16 @@
 app.controller("ctrl", function($scope) {
+
+    function Message(txt){
+
+        this.createdBy=message.createdBy;
+        this.createdAt=message.createdAt;
+        this.title=message.title;
+        this.details=message.details;
+        this.priority=message.priority;
+        this.comments=message.comments;
+
+    }
+
+    
    
 });
