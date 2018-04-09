cdf6b7de89aedb92366a93c509cd5acfff3073d6
qawqawzse <qawqawzse@gmail.com>
2018-04-09 12:58:57 +0300

    login and users data start

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var app=angular.module("myApp",["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "app/messages/messages.html",
-        controller : "messagesCtrl"
+        templateUrl : "app/login/login.html",
+        controller : "loginCtrl"
      
         
         
---
+++ app/users/userService.js
@@ -0,0 +1,37 @@
+app.factory("activeUserService", function ($http, $log, $q) {
+    
+    function User(user) {
+        this.email = user.email;
+        this.password = user.password;
+        this.fname = user.fname;
+        this.lname = user.lname;
+        this.data = user.data;
+    }
+
+
+    
+    
+    function loadUsers(){
+
+
+
+    }
+    
+    
+    function newUser(user){
+
+
+    }
+
+    function userLogin(user) {
+
+
+    }
+
+
+
+
+
+
+
+});    
