db261c919ae4816dd6e56f57372b873247746a73
qawqawzse <qawqawzse@gmail.com>
2018-04-21 17:10:28 +0300

    homepage deisgn removed to login page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app=angular.module("myApp",["ngRoute"]);
 
 app.config(function($routeProvider) {
     $routeProvider
-    .when("/", {
+    .when("/home", {
         templateUrl : "app/home/home.html",
         controller : "homeCtrl"
     
