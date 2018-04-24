2da751c3ff425b9681882bb9da9b6ea360f6bdd6
qawqawzse1 <qawqawzse@gmail.com>
2018-04-24 12:21:36 +0300

    active user widget is working

--- ctrl.js
+++ ctrl.js
@@ -1,13 +1,43 @@
 app.controller("ctrl", function($scope, userService, $location) {
 
 
+    $scope.activeUser={};
+
+
 $scope.isHomeScreen= function() {
 
-    if ($location.path("/")){
+    if ($location.path() === "/"){
         
         return true;
     }
  }    
 
+
+ $scope.getActiveUserImage=function(){
+
+    $scope.activeUser=userService.loggedIn();
+
+    return $scope.activeUser.img;
+    
+ }
+
+ $scope.getActiveUserName=function(){
+
+    $scope.activeUser=userService.loggedIn();
+    
+        return $scope.activeUser.fname +" "+ $scope.activeUser.lname ;
+        
+
+ }
+
+ $scope.activeUserShow=function(){
+
+    if($scope.activeUser!= null || undefined || false){
+
+        return true;
+    }else {
+        return false;
+    }
+ }
    
 });
